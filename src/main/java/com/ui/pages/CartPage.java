c869e63ef3d032f75ebc454f198dc6dc510bbf6d
Mangesh <mangesh.bharati01@gmail.com>
2025-06-01 10:01:08 +0530

    new changes in cartpage

--- src/main/java/com/ui/pages/CartPage.java
+++ src/main/java/com/ui/pages/CartPage.java
@@ -3,8 +3,14 @@ package com.ui.pages;
 public class CartPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int i =10;
+		System.out.println(i);
 
+	}
+	
+	public static void cartPage() {
+		System.out.println("cart page");
 	}
 
 }

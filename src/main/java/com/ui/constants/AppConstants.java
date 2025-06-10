e596dcd71f834fc1dd6f807b99814fce9096f2ca
Mangesh <mangeshmbharati45@gmail.com>
2025-06-10 07:56:29 +0530

    corrected AppConstants

--- src/main/java/com/ui/constants/AppConstants.java
+++ src/main/java/com/ui/constants/AppConstants.java
@@ -8,7 +8,7 @@ public class AppConstants {
 	public static final int MEDIUM_DEFAULT_TIMEOUT = 10;
 	public static final int LONG_DEFAULT_TIMEOUT = 20;
 	
-	public static final String HOME_PAGE_TITLE = "My Account11";
+	public static final String HOME_PAGE_TITLE = "My Account";
 	public static final CharSequence REGISTER_SUCCESS_MESSG = "Your Account Has Been Created!";
 	
 	public static List<String> expectedAccountPageHeaders = List.of("My Account", 
